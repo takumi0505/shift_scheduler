2988c9cf6e79965a28a74d7debe4437337fdf284
takumi0505 <takumi.teshima0529@gmail.com>
2024-06-07 23:49:35 +0900

    シフト登録画面での色の割り当てをした

--- src/main/java/com/example/shift_scheduler/ShiftSchedulerApplication.java
+++ src/main/java/com/example/shift_scheduler/ShiftSchedulerApplication.java
@@ -1,7 +1,9 @@
 package com.example.shift_scheduler;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ShiftSchedulerApplication {
@@ -9,5 +11,6 @@ public class ShiftSchedulerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShiftSchedulerApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/example/shift_scheduler/service/UserService.java
+++ src/main/java/com/example/shift_scheduler/service/UserService.java
@@ -1,6 +1,5 @@
 package com.example.shift_scheduler.service;
 
-
 import com.example.shift_scheduler.entity.User;
 import com.example.shift_scheduler.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class UserService {
@@ -15,11 +15,20 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final String[] COLORS = {
+            "#FF5733", "#33FF57", "#3357FF", "#FF33A5", "#A533FF",
+            "#FF8F33", "#33FFCE", "#FF3333", "#8FFF33", "#5733FF",
+            "#33FFA5", "#FF33CE", "#33A5FF", "#A5FF33", "#FF5733",
+            "#57FF33", "#FF3357", "#33FF8F", "#5733FF", "#FF5733"
+    };
+
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     public void saveUser(User user) {
+        String color = assignColor();
+        user.setColor(color);
         userRepository.save(user);
     }
 
@@ -31,4 +40,19 @@ public class UserService {
         Optional<User> optionalUser = userRepository.findById(id);
         return optionalUser.orElseThrow(() -> new RuntimeException("User not found"));
     }
+
+    private String assignColor() {
+        List<User> users = userRepository.findAll();
+        for (String color : COLORS) {
+            boolean colorUsed = users.stream().anyMatch(user -> color.equals(user.getColor()));
+            if (!colorUsed) {
+                return color;
+            }
+        }
+        // もし全ての色が使用されている場合、ランダムに選択する
+        Random random = new Random();
+        return COLORS[random.nextInt(COLORS.length)];
+    }
+
+
 }
